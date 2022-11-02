ad5ee8c40b46b584d8479c87198f745d7fc97bac
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-11-02 13:24:19 -0700

    slide

---
+++ webAssets.js
@@ -0,0 +1,3 @@
+var slide = document.getElementsByClassName("slide");
+slide.document.head.append(slide);
+slide.innerHTML = ".slide {" + "height: " + window.innerHeight + "px;" + "width: " + window.innerWidth + "px;" + "}"

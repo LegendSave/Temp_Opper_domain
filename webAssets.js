87456fb89775efd8ca45dc8e9e6f27c2097cb188
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-11-03 08:54:06 -0700

    web

--- webAssets.js
+++ webAssets.js
@@ -1,5 +1,12 @@
-var slide = document.querySelector('body');
+var slide = document.getElementById('slide');
+slide.height = window.innerHeight;
+slide.width = window.innerWidth;
+var c = slide.getContext("2d");
 
 function fullScreen(){
 slide.requestFullscreen();
 }
+
+c.beginPath();
+c.fillStyle = "white";
+c.fillRect(0, 0, slide.width, slide.height);

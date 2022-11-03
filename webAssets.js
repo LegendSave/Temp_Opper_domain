7fb6513f8dc05ddc3a55fa6651aaaebc6fe2c79f
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-11-03 00:02:37 -0700

    Fix

--- webAssets.js
+++ webAssets.js
@@ -1,3 +1,5 @@
-var slide = document.getElementsByClassName("slide");
-slide.document.head.append(slide);
-slide.innerHTML = ".slide {" + "height: " + window.innerHeight + "px;" + "width: " + window.innerWidth + "px;" + "}"
+var slide = document.querySelector('body');
+
+function fullScreen(){
+slide.requestFullscreen();
+}

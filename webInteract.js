b1c556b893e60bccf0aaf2da6fbaffc77422bf6c
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-11-07 11:49:59 -0800

    webinteract

--- webInteract.js
+++ webInteract.js
@@ -10,4 +10,10 @@ c.font = '10px Arial';
 
 var cords = [`${window.innerWidth - (window.innerWidth / 3)}`, `${window.innerWidth / 3}`];
 
-c.fillText(cords[1], 300, 300);
+c.beginPath();
+c.strokeStyle = "black";
+c.moveTo(cords[0], 0);
+c.lineTo(cords[0], window.innerHeight);
+c.moveTo(cords[1], 0);
+c.lineTo(cords[1], window.innerHeight);
+c.stroke();

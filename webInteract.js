3945fce592b6b30c478a380caf91dd7cfcb8bc2a
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-11-07 12:45:04 -0800

    e

--- webInteract.js
+++ webInteract.js
@@ -8,6 +8,7 @@ canvas.height = window.innerHeight;
 c.scale(scale, scale);
 c.font = '10px Arial';
 
-var cords = [window.innerWidth / 3, window.innerWidth - scaling];
+var cord1 = window.innerWidth / 3;
+var cord2 = window.innerWidth - scaling;
 c.fillRect(cords[1], 200, 20, 20);
-c.fillRect(cords[0], 200, 20, 20);
+c.fillRect(cords[2], 200, 20, 20);

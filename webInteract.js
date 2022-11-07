792061d6fd0508790d10800519f5aef0cb22fc79
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-11-07 12:36:22 -0800

    e

--- webInteract.js
+++ webInteract.js
@@ -1,14 +1,13 @@
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 window.devicePixelRatio = 2;
-
+var scaling = window.innerWidth / 3;
 var scale = window.devicePixelRatio; 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 c.scale(scale, scale);
 c.font = '10px Arial';
 
-var cords = [window.innerWidth / 3, window.innerWidth - (window.innerWidth / 3)];
-
+var cords = [window.innerWidth / 3, window.innerWidth - scaling];
 c.fillRect(cords[1], 200, 20, 20);
-c.fillRect(cords[0], 200, 20, 20);
+c.fillText(cords[2], 200, 20, 20);

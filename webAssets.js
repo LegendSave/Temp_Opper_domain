ac024811d5ab46b32308ee2a41356da3d3d63e88
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-11-07 11:35:13 -0800

    bug fix

--- webAssets.js
+++ webAssets.js
@@ -4,15 +4,3 @@ function fullScreen(){
 slide.requestFullscreen();
 button.style.display = 'none';
 }
-var slide = document.getElementById('slide');
-slide.height = window.innerHeight;
-slide.width = window.innerWidth;
-var c = slide.getContext("2d");
-
-function fullScreen(){
-slide.requestFullscreen();
-}
-
-c.beginPath();
-c.fillStyle = "green";
-c.fillRect(0, 0, slide.width, slide.height);

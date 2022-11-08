3781a8fd7953ff0545d16fcc08f57c65b3898671
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-11-07 22:12:54 -0800

    Update webInteract.js

--- webInteract.js
+++ webInteract.js
@@ -1,14 +1,36 @@
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
-window.devicePixelRatio = 2;
-var scaling = window.innerWidth / 3;
+window.devicePixelRatio = 1;
 var scale = window.devicePixelRatio; 
+var mouseX;
+var mouseY;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 c.scale(scale, scale);
-c.font = '10px Arial';
+c.font = '50px Arial';
+var cord1 = canvas.width / 3;
+var cord2 = cord1 + cord1;
+c.fillRect(cord1, 200, 20, 20);
+c.fillRect(cord2, 200, 20, 20);
 
-var cord1 = window.innerWidth / 3;
-var cord2 = window.innerWidth - scaling;
-c.fillRect(cords[1], 200, 20, 20);
-c.fillRect(cords[2], 200, 20, 20);
+document.addEventListener('mousemove', e => {
+mouseX = e.screenX;
+mouseY = e.screenY;
+});
+
+function draw(){
+    requestAnimationFrame(draw);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    if (Math.abs(mouseX) < cord1){
+        c.fillText("Left", canvas.width / 2, 200);
+    }
+    if (Math.abs(mouseX) > cord2){
+        c.fillText("Right", canvas.width / 2, 200);
+    }
+    c.font = '50px Arial';
+    c.fillText("Title", 20, 20);
+    c.font = '25px Arial';
+    c.fillText("Content", 20, 50);
+}
+
+draw();

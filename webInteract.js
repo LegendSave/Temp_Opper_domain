0abe93600b02bb64af842eab0ed46c4252b5972f
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-11-08 23:02:19 -0800

    Quality Update

--- webInteract.js
+++ webInteract.js
@@ -8,6 +8,8 @@ var mouseInput;
 var keyInput;
 var slideNum = 0;
 var slideContent = ["1st content", "2nd content"];
+var slideImages = [document.getElementById("img1"), document.getElementById("img2")];
+var textValue;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 c.scale(scale, scale);
@@ -24,13 +26,51 @@ document.addEventListener('mousedown', e => {
 mouseInput = e.returnValue;
 });
 
-document.addEventListener('keypress', e => {
-keyInput = e.value;
+document.addEventListener('keydown', e => {
+keyInput = e.keyCode;
 });
+
+//Double spaces document, used for MLA format.
+function dblSpc(startText, fontSize) {
+    this.startText = startText;
+    this.fontSize = fontSize;
+    fontSize = fontSize * 2;
+    let value = startText + fontSize;
+    return value;
+}
+
+//Generates a link for the user to click on, Used for works sited.
+function link(text, x, y, fontSize, color, url){
+    this.text = text;
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.color = color;
+    this.url = url;
+
+    let textLength = c.measureText(text).width;
+    let height = (y + fontSize) - 20;
+    let fontSz = `${fontSize}` + 'px';
+    c.beginPath();
+    c.font = `${fontSz} Times New Roman`;
+    c.fillStyle = color;
+    c.strokeStyle = color;
+    c.fillText(text, x, y);
+    c.moveTo(x, height);
+    c.lineTo(x + textLength, height);
+    c.stroke();
+    c.fill();
+
+    //Add conditionals to read mouse position relative to the text.
+} 
 
 function draw(){
     requestAnimationFrame(draw);
     c.clearRect(0, 0, canvas.width, canvas.height);
+    c.shadowColor = "black";
+    c.shadowBlur = 5;
+    c.shadowOffsetX = -5;
+    c.shadowOffsetY = -5;
     if (Math.abs(mouseX) < cord1 && mouseInput || keyInput == 37){
         c.fillText("Left", canvas.width / 2, 200);
         if (slideNum == 0){
@@ -64,10 +104,17 @@ function draw(){
         mouseInput = false;
     }
     c.fillStyle = "black";
-    c.font = '50px Arial';
+    c.font = '50px Times New Roman';
     c.fillText("Title", 20, 20);
-    c.font = '25px Arial';
-    c.fillText(slideContent[slideNum], 20, 50);
+    c.font = '25px Times New Roman';
+    c.fillText(slideContent[slideNum], 20, dblSpc(20, 25));
+    //Edit code before checking if link works.
+    link("Random url", 20, dblSpc(dblSpc(20, 25), 25), 25, "blue");
+    c.shadowColor = "black";
+    c.shadowBlur = 10;
+    c.shadowOffsetX = 10;
+    c.shadowOffsetY = 10;
+    //c.drawImage(slideImages[slideNum], 80, 80);
 }
 
 draw();

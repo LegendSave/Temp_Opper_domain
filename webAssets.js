7f9d9171208e7057bbc2d6a09462143832cbb6f7
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-11-28 00:43:08 -0800

    Animation polishing

--- webAssets.js
+++
@@ -1,6 +0,0 @@
-var fullscreenContent = document.getElementById("fullscreenContent");
-var button = document.querySelector('button');
-function fullScreen(){
-fullscreenContent.requestFullscreen();
-button.style.display = 'none';
-}
